d553efce20ca5e23e7edacdad324033aa7a33547
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-05-24 22:46:47 +0600

    solve 2nd problem

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -52,3 +52,8 @@ SELECT * FROM rangers;
 -- Problem 1️⃣
 INSERT INTO rangers (name, region)
 VALUES ('Derek Fox', 'Coastal Plains');
+
+-- Problem 2️⃣
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
+
