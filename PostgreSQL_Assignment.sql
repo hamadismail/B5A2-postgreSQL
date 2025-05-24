a3e0942044d1e811e89ad694799d94f7dfe07c7f
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-05-24 22:44:02 +0600

    solve 1st problem

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,47 +1,54 @@
 -- Create Database
 CREATE DATABASE conservation_db;
 
+-- DROP TABLE rangers, species, sightings;
+
 -- Create Tables
 CREATE TABLE rangers (
-    ranger_id INT PRIMARY KEY,
-    name VARCHAR(50),
-    region VARCHAR(50)
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    region VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE species (
-    species_id INT PRIMARY KEY,
-    common_name VARCHAR(50),
-    scientific_name VARCHAR(50),
-    discovery_date DATE,
-    conservation_status VARCHAR(50)
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name VARCHAR(50) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE sightings (
-    sighting_id INT PRIMARY KEY,
-    species_id INT,
-    ranger_id INT,
-    location VARCHAR(50),
-    sighting_time TIMESTAMP,
-    notes TEXT,
-    FOREIGN KEY (species_id) REFERENCES species(species_id),
-    FOREIGN KEY (ranger_id) REFERENCES rangers(ranger_id)
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INT REFERENCES species(species_id),
+    ranger_id INT REFERENCES rangers(ranger_id),
+    location VARCHAR(100) NOT NULL,
+    sighting_time TIMESTAMP NOT NULL,
+    notes TEXT
 );
 
 -- Insert Values Into the Tables
-INSERT INTO rangers (ranger_id, name, region) VALUES
-(1, 'Alice Green', 'Northern Hills'),
-(2, 'Bob White', 'River Delta'),
-(3, 'Carol King', 'Mountain Range');
+INSERT INTO rangers (name, region) VALUES
+('Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range');
 
-INSERT INTO species (species_id, common_name, scientific_name, discovery_date, conservation_status) VALUES
-(1, 'Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
-(2, 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
-(3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
-(4, 'Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
-INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_time, notes) VALUES
-(1, 1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
-(2, 2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
-(3, 3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
-(4, 1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
 
+SELECT * FROM rangers;
+-- DELETE FROM rangers
+-- WHERE ranger_id = 6
+
+-- Problem 1️⃣
+INSERT INTO rangers (name, region)
+VALUES ('Derek Fox', 'Coastal Plains');
