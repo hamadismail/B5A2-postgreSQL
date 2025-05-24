22089fd80908b726ec512cf8fd05a74073978af5
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-05-24 22:04:16 +0600

    create tables

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,4 +1,28 @@
 -- Create Database
 CREATE DATABASE conservation_db;
 
+-- Create Tables
+CREATE TABLE rangers (
+    ranger_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    region VARCHAR(50)
+);
 
+CREATE TABLE species (
+    species_id INT PRIMARY KEY,
+    common_name VARCHAR(50),
+    scientific_name VARCHAR(50),
+    discovery_date DATE,
+    conservation_status VARCHAR(50)
+);
+
+CREATE TABLE sightings (
+    sighting_id INT PRIMARY KEY,
+    species_id INT,
+    ranger_id INT,
+    location VARCHAR(50),
+    sighting_time DATE,
+    notes TEXT,
+    FOREIGN KEY (species_id) REFERENCES species(species_id),
+    FOREIGN KEY (ranger_id) REFERENCES rangers(ranger_id)
+);
